5e4d200a8ef3155a1bd7a210249762ab40499896
Roopesh Chander <roop@roopc.net>
2021-08-17 12:28:14 +0530

    Comment out the filtering out code

--- search.js
+++ search.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // if we already have a favorite IdP, we need to focus on the input search
     // box that becomes visible when the user toggles <details>
-    if (null !== document.querySelector("details")) {
+    /*if (null !== document.querySelector("details")) {
         var details = document.querySelector("details");
         details.addEventListener("toggle", function(e) {
             if (this.open) {
@@ -11,9 +11,9 @@ document.addEventListener("DOMContentLoaded", function() {
                 searchBoxInput.focus();
             }
         });
-    }
+    }*/
 
-    /*if (null !== document.querySelector("form#searchBox")) {
+    if (null !== document.querySelector("form#searchBox")) {
         var searchBox = document.querySelector("form#searchBox");
         // unhide the search box
         searchBox.style.display = "block";
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
             // disable default form submit
             e.preventDefault();
         });
-        var searchBoxInput = document.querySelector("form#searchBox input");
+        /*var searchBoxInput = document.querySelector("form#searchBox input");
         searchBoxInput.focus();
         searchBoxInput.addEventListener("keyup", function() {
             var searchFor = this.value.toUpperCase();
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 document.querySelector("span#noResults").style.display = "none";
             }
-        });
-    }*/
+        });*/
+    }
 });
 
